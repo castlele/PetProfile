ba136cdcd4a46c0368457198d0f87c482b82fabd
castlele <castlelecs@gmail.com>
2024-10-12 07:28:50 +0300

    [feat]: add logging to pets profile view model

--- pet-profile-utils/src/commonMain/kotlin/com/castlelecs/utils/logger/Logger.kt
+++ pet-profile-utils/src/commonMain/kotlin/com/castlelecs/utils/logger/Logger.kt
@@ -3,3 +3,19 @@ package com.castlelecs.utils.logger
 interface Logger {
     fun log(level: LogLevel, message: String)
 }
+
+fun Logger.debug(message: String) {
+    log(LogLevel.DEBUG, message)
+}
+
+fun Logger.info(message: String) {
+    log(LogLevel.INFO, message)
+}
+
+fun Logger.warn(message: String) {
+    log(LogLevel.WARN, message)
+}
+
+fun Logger.error(message: String) {
+    log(LogLevel.ERROR, message)
+}
